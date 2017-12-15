12a7158dab398e0c262161cf1adb979d7b9cf3e5
Kris <christellelocque@hotmail.fr>
2017-12-15 17:02:32 +0100

    Avancement de l'exercice et reparation du fichier gulpfile.

---
+++ assets/js/app.js
@@ -0,0 +1,95 @@
+//API Keys 7b721f0a9e9d92c7b2312b26d8a6bdfb
+
+// ajaxGet("https://www.openweathermap.org/current", function (reponse)
+
+console.log('connect')
+
+$.ajax({
+    url: 'http://api.openweathermap.org/data/2.5/weather?q=cahors&units=metric&APPID=7b721f0a9e9d92c7b2312b26d8a6bdfb',
+    type: 'GET',
+    dataType: 'json',
+})
+.done(function(res) {
+    var city = (res.name)
+    var coordlon = (res.coord.lon)
+    var coordlat = (res.coord.lat)
+    var temp = (res.main.temp)
+    var tempmin = (res.main.temp_min)
+    var tempmax = (res.main.temp_max)
+    var pressure = (res.main.pressure)
+    var humid = (res.main.humidity)
+    var wind = (res.wind.speed)
+    
+    console.log(city, temp, tempmin, tempmax, pressure, humid);
+    
+    $('#city').html('City: '+city);
+    $('#place').html('GPS Lon: '+coordlon+ ' Lat: '+coordlat);
+    $('#temp').html('Température: '+temp+'°C / Min: '+tempmin+'°C / Max: '+tempmax+'°C');
+    $('#pression').html('Pressure: '+pressure+' hPa');
+    $('#humid').html('Humidity: '+humid+'%');
+    $('#wind').html('Wind speed: '+wind+' m/sec');
+
+    var map = L.map('map1').setView([51.505, -0.09], 13);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    
+    L.marker([51.5, -0.09]).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+})
+
+.fail(function(err) {
+    console.log(err);
+})
+
+.always(function() { // S'execute dans tous les cas!
+console.log("complete");
+});
+
+
+
+$("#bouton").click(function (formulaire) {
+    var requete = $('#search').val();
+    // var requete = document.querySelector('#search').value
+    console.log(requete)
+    $.ajax({
+        url: 'http://api.openweathermap.org/data/2.5/weather?q='+requete+'&units=metric&APPID=7b721f0a9e9d92c7b2312b26d8a6bdfb',
+        type: 'GET',
+        dataType: 'json',
+    })
+    .done(function(res) {
+        var city = (res.name)
+        var coordlon = (res.coord.lon)
+        var coordlat = (res.coord.lat)
+        var temp = (res.main.temp)
+        var tempmin = (res.main.temp_min)
+        var tempmax = (res.main.temp_max)
+        var pressure = (res.main.pressure)
+        var humid = (res.main.humidity)
+        var wind = (res.wind.speed)
+        
+        console.log(city, temp, tempmin, tempmax, pressure, humid);
+        
+        $('#city2').html('City: '+city);
+        $('#place2').html('GPS Lon: '+coordlon+ ' Lat: '+coordlat);
+        $('#temp2').html('Température: '+temp+'°C / Min: '+tempmin+'°C / Max: '+tempmax+'°C');
+        $('#pression2').html('Pressure: '+pressure+' hPa');
+        $('#humid2').html('Humidity: '+humid+'%');
+        $('#wind2').html('Wind speed: '+wind+' m/sec');
+
+        var map = L.map('map2').setView([51.505, -0.09], 13);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
+    })
+       
+});
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,34 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
-var pump = require('pump');
-var $ = require('gulp-load-plugins')();
 var browserSync = require('browser-sync').create();
-var reload = browserSync.reload;
-var scsslint = require('gulp-scss-lint');
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   return gulp.src('assets/scss/style.scss')
-    .pipe($.sass({
-      includePaths: 'assets/scss/components',
-
-    })
-    .on('error', $.sass.logError))
+    .pipe(sass())
     .pipe(gulp.dest('dist/css'))
-    .pipe(browserSync.stream());
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
 gulp.task('uglify', function () {
-        gulp.src('assets/js/app.js')
-          uglify()
-        gulp.dest('dist/js/')
-        .pipe(browserSync.stream());
+  gulp.src('assets/js/app.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js/'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
 
-gulp.task('serve', ['sass', 'uglify'], function() {
-    browserSync.init({
-        server: './dist/',
-        open: true,
-        notify: false,
-        reload:true
-    });
-		gulp.watch('assets/scss/**/*.scss',['sass']);
-		gulp.watch('assets/js/**/*.js', ['uglify']);
-		 gulp.watch("dist/*.html").on('change', browserSync.reload);
+gulp.task('serve', ['sass', 'uglify'], function () {
+  browserSync.init({
+    server: './dist/'
+  });
+  gulp.watch('assets/scss/*.scss', ['sass']);
+  gulp.watch('assets/js/*.js', ['uglify']);
+  gulp.watch("dist/*.html", browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
